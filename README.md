6cb0d9c02203ca57c4dddf7ba1a4215d0cce1313
JoãoOspedal <jpospedal25@gmail.com>
2026-04-10 15:13:37 -0300

    Add base archives and configs

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Portfolio
-This is my professional portfolio, made with HTML5, CSS3, JavaScript and Next.js
+This is my professional Portfolio, made with HTML5, CSS3, JavaScript and Next.js
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import header from 'Portfolio/components/header';
+import hero from 'Portfolio/components/hero';
+import projects from 'Portfolio/components/projects';
+import contact from 'Portfolio/components/contact';
+
+export default function Home() {
+    console.log('Hello, Next.js!');
+}
